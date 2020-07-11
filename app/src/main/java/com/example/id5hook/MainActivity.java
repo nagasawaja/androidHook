58fb1178a122ebaf522125af3a9301b8a218f7a5
huangliangxu <411030589@qq.com>
2020-07-11 17:36:58 +0800

    android6 add resume

--- app/src/main/java/com/example/id5hook/MainActivity.java
+++ app/src/main/java/com/example/id5hook/MainActivity.java
@@ -9,6 +9,10 @@ import android.util.Log;
 import java.io.IOException;
 
 import de.robv.android.xposed.XposedBridge;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private HttpApi httpapi;
@@ -28,5 +32,29 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
             Log.e("onResume", "WebServer start failed" + e.getMessage());
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("benija", "eeeeee");
+        String url = "http://www.beni.pub/collyMao/getIp";
+        OkHttpClient okHttpClient = new OkHttpClient();
+        final Request request = new Request.Builder()
+                .url(url)
+                .build();
+        final Call call = okHttpClient.newCall(request);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Response response = call.execute();
+                    Log.d("benija", "run: " + response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
     }
 }
